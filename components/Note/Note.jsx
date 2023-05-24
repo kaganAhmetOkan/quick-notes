6b2fdf9cddc28f1c8a1675a47ed094e44015b661
Kagan Ahmet Okan <kaganAhmetOkan@gmail.com>
2023-05-25 02:51:38 +0300

    Style changes in Notes && Note components

--- components/Note/Note.jsx
+++ components/Note/Note.jsx
@@ -6,26 +6,22 @@ import Link from "next/link";
 export default function Note({ note }) {
   
   const defaultNote = (
-    <div className={style.container}>
-      <Link className={style.main} href={`/note/${note.id}`}>
-          <h4>{note.title}</h4>
-          <p>{note.content}</p>
-          <div className={style.date}>{note.date}</div>
-      </Link>
+    <div className={style.main}>
+        <h4>{note.title}</h4>
+        <p>{note.content}</p>
+        <Link href={`/note/${note.id}`}>View</Link>
     </div>
   );
 
   const createNewNote = (
-    <div className={style.container}>
-      <Link className={style.main} href={"/new"}>
-        <h4>{note.title}</h4>
-        <div className={style.image}>
-          <Image
-            alt="plus"
-            src={iconPlus}
-            height={48} width={48}
-          />
-        </div>
+    <div className={style.main}>
+      <h4>{note.title}</h4>
+      <Link className={style.image} href="/new">
+        <Image
+          alt="plus"
+          src={iconPlus}
+          height={48} width={48}
+        />
       </Link>
     </div>
   );
